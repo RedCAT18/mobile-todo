99afcc57c394bc8c4b4b52884b6f764258cad709
RedCAT <akaineko18@gmail.com>
2018-03-15 13:19:25 +1300

    App Stylising

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, StatusBar, Dimensions, Platform } from 'react-native';
+
+const { height, width } = Dimensions.get("window");
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" backgroundColor='transparent'/>
+        <Text style={styles.title}> To Do </Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={'Enter new schedule.'}/>
+        </View>
       </View>
     );
   }
@@ -14,8 +20,41 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#aaf0d0',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 42,
+    fontWeight: '100',
+    color: 'white',
+    marginTop: 50,
+    marginBottom: 30,
+  },
+  card: {
+    backgroundColor: 'white',
+    flex: 1,
+    width: width -25,
+    padding: 10,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    //shadow는 ios와 안드로이드가 다름
+    ...Platform.select({
+      ios: {
+        shadowColor: 'rgb(50, 50, 50)',
+        shadowOpacity: 0.5,
+        shadowRadius: 5,
+        shadowOffset: {
+          height: -1,
+          width: 0
+        }
+      },
+      android: {
+        elevation: 3
+      }
+    }),
+  },
+  input: {
+    padding: 5,
+    height: 40, 
   },
 });
